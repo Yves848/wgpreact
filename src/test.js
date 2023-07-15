6e7d21cbb91c2290e293330a58ebbbcc9e7d3b60
yves848 <yves.godart@gmail.com>
2023-07-15 07:36:59 +0200

    commit

---
+++ src/test.js
@@ -0,0 +1,64 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var child_process_1 = require("child_process");
+//import path, { resolve } from "path/posix";
+function start() {
+    //spawnSample('cmd.exe',['winget', '-v']);
+    execSample('winget list');
+}
+function spawnSample(command, args) {
+    var execProcess = (0, child_process_1.spawn)(command, args);
+    console.log('spawn');
+    console.log(execProcess.spawnfile);
+    execProcess.on('spawn', function () {
+        console.log('spawn on spawn');
+    });
+    execProcess.stdout.on('data', function (data) {
+        //var sd: string = (data as string);
+        //if (sd.match('winget')) {
+        console.log("spawn stdout: ".concat(data));
+        //}
+    });
+    execProcess.stderr.on('data', function (data) {
+        console.log("spawn on error ".concat(data));
+    });
+    execProcess.on('exit', function (code, signal) {
+        console.log("spawn on exit code: ".concat(code, " signal: ").concat(signal));
+    });
+    execProcess.on('close', function (code, args) {
+        console.log("spawn on close code: ".concat(code, " args: ").concat(args));
+    });
+}
+function execSample(args) {
+    var execProcess = (0, child_process_1.exec)(args, { 'encoding': 'utf8' }, function (error, stdout) {
+        var s = [];
+        s = stdout.split('\r\n');
+        s.forEach(function (line, i) {
+            if (line.match('----------')) {
+                console.log(s[i - 1]);
+            }
+            if (line.match('winget$')) {
+                console.log(line);
+            }
+        });
+    });
+    //console.log('exec spawn');
+    //console.log(execProcess.spawnfile);
+    execProcess.on('spawn', function () {
+        //console.log('exec on spawn');
+    });
+    execProcess.on('error', function (err) {
+        //console.log(`exec on error:${err}`);
+    });
+    execProcess.on('exit', function (code, signal) {
+        //console.log(`exec on exit code: ${code} signal: ${signal}`);
+    });
+    execProcess.on('close', function (code, args) {
+        //console.log(`exec on close code: ${code} args: ${args}`);
+    });
+}
+start();
+// const { exec } = require('child_process');
+// exec('winget search code', (error:any, stdout:string, stderr:any)=> {
+//     console.log(stdout);
+// })
---
+++ src/test.ts
@@ -0,0 +1,67 @@
+import { exec, execFile, fork, spawn } from "child_process";
+//import path, { resolve } from "path/posix";
+
+function start() {
+    //spawnSample('cmd.exe',['winget', '-v']);
+    execSample('winget list');
+}
+function spawnSample(command: string, args: string[]) {
+    const execProcess = spawn(command, args);
+    console.log('spawn');
+    console.log(execProcess.spawnfile);
+    execProcess.on('spawn', () => {
+        console.log('spawn on spawn');
+    });
+    execProcess.stdout.on('data', (data) => {
+        //var sd: string = (data as string);
+        //if (sd.match('winget')) {
+          console.log(`spawn stdout: ${data}`);
+        //}
+        
+    });
+    execProcess.stderr.on('data', (data) => {
+        console.log(`spawn on error ${data}`);
+    });
+    execProcess.on('exit', (code, signal) => {
+        console.log(`spawn on exit code: ${code} signal: ${signal}`);
+    });
+    execProcess.on('close', (code: number, args: any[])=> {
+        console.log(`spawn on close code: ${code} args: ${args}`);
+    });
+}
+
+function execSample(args: string) {
+  const execProcess = exec(args, { 'encoding': 'utf8' }, (error, stdout) => {
+      var s : string[] = [];
+      s = (stdout as string).split('\r\n');
+      s.forEach((line,i) => {
+        if (line.match('----------')) {
+          console.log(s[i-1]);
+        }
+        if (line.match('winget$')) {
+          console.log(line);
+        }
+      })
+  });
+  //console.log('exec spawn');
+  //console.log(execProcess.spawnfile);
+  execProcess.on('spawn', () => {
+      //console.log('exec on spawn');
+  });
+  execProcess.on('error', (err) => {
+      //console.log(`exec on error:${err}`);
+  });
+  execProcess.on('exit', (code, signal) => {
+      //console.log(`exec on exit code: ${code} signal: ${signal}`);
+  });
+  execProcess.on('close', (code: number, args: any[])=> {
+      //console.log(`exec on close code: ${code} args: ${args}`);
+  });
+}
+start();
+
+
+// const { exec } = require('child_process');
+// exec('winget search code', (error:any, stdout:string, stderr:any)=> {
+//     console.log(stdout);
+// })
